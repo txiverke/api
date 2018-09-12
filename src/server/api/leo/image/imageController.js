53616dbf410a084d296a1ed74b7ce152a52baae6
txiverke <txiverke@gmail.com>
2018-09-12 12:30:39 -0400

    Update v: 1.0.5 - Fix Image Model fiter by year

--- src/server/api/leo/image/imageController.js
+++ src/server/api/leo/image/imageController.js
@@ -1,5 +1,10 @@
 import Image from './imageModel'
 
+/**
+ * List all the images
+ * @param {*} req 
+ * @param {*} res 
+ */
 export const list = async (req, res) => {
   try{
     const images = await Image.find({})
@@ -9,6 +14,11 @@ export const list = async (req, res) => {
   }
 }
 
+/**
+ * Create and save a new Image Item
+ * @param {*} req 
+ * @param {*} res 
+ */
 export const create = async (req, res) => {
   try {
     const newImage = new Image(req.body)
@@ -20,8 +30,26 @@ export const create = async (req, res) => {
   }
 }
 
-export const read = (req, res) => res.status(200).json(req.image)
+/**
+ * Return an specific image by id
+ * @param {*} req 
+ * @param {*} res 
+ */
+export const read = (req, res) => res.status(200).json({ success: true, data: req.image })
 
+/**
+ * Return a collection of Images by year
+ * @param {*} req 
+ * @param {*} res 
+ */
+export const yearList = (req, res) => res.status(200).json({ success: true, data: req.image })
+
+
+/**
+ * Update Image Model data
+ * @param {*} req 
+ * @param {*} res 
+ */
 export const update = async (req, res) => {
   try {
     const updateImage = Object.assign(req.image, req.body)
@@ -33,11 +61,13 @@ export const update = async (req, res) => {
   }
 }
 
-export const yearList = (req, res) => {
-  const images = req.image
-  return res.status(200).send(images)
-}
-
+/**
+ * Filter by Id
+ * @param {*} req 
+ * @param {*} res 
+ * @param {*} next 
+ * @param {*} id 
+ */
 export const imageById = async (req, res, next, id) => {
   try {
 
@@ -48,6 +78,13 @@ export const imageById = async (req, res, next, id) => {
   }
 }
 
+/**
+ * Filter by year
+ * @param {*} req 
+ * @param {*} res 
+ * @param {*} next 
+ * @param {*} year 
+ */
 export const imageByYear = async (req, res, next, year) => {
   try {
     req.image = await Image.find({'year': year})
